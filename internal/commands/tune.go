01e653a265ce18e911eda98ce99cd3c639c6f4e7
Lars Kellogg-Stedman <lars@oddbit.com>
2025-11-13 11:39:42 -0500

    Only set vfo when there are changes; add --force flag

--- internal/commands/tune.go
+++ internal/commands/tune.go
@@ -29,17 +29,33 @@ func (c TuneCommand) Run(r *radio.Radio, ctx config.Context, args []string) (str
 	if err != nil {
 		return "", fmt.Errorf("failed to parse vfo configuration: %w", err)
 	}
+	oldVfo := vfo
 
+	var forceVfoMode bool
 	flags := flag.NewFlagSet("id", flag.ContinueOnError)
+	flags.BoolVarP(&forceVfoMode, "force", "f", false, "change to vfo mode before tuning")
 	flags.VarP(types.NewFrequencyMHz(&vfo.RxFreq), "rxfreq", "r", "frequency in MHz (e.g., 144.39)")
+	flags.VarP(types.NewStepSize(&vfo.RxStep), "rxstep", "s", "step size in hz (e.g., 5)")
 	if err := flags.Parse(args); err != nil {
 		return "", fmt.Errorf("command failed: %w", err)
 	}
 
-	res, err = r.SendCommand("FO", strings.Split(vfo.Serialize(), ",")...)
-	if err != nil {
-		return "", fmt.Errorf("failed to tune vfo")
+	if vfo != oldVfo {
+		if forceVfoMode {
+			_, err := r.SendCommand("VM", ctx.Config.Vfo, "0")
+			if err != nil {
+				return "", fmt.Errorf("failed to change to vfo mode: %w", err)
+			}
+		}
+		res, err = r.SendCommand("FO", strings.Split(vfo.Serialize(), ",")...)
+		if err != nil {
+			return "", fmt.Errorf("failed to tune vfo: %w", err)
+		}
 	}
 
+	vfo, err = types.ParseVFO(res)
+	if err != nil {
+		return "", fmt.Errorf("failed to parse vfo configuration: %w", err)
+	}
 	return fmt.Sprintf("%s\n", vfo), nil
 }
