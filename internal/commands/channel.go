d78185c7e33293eae5b3d1a8b17e4f732c3b98d5
Lars Kellogg-Stedman <lars@oddbit.com>
2025-11-13 15:14:24 -0500

    Add channel-list command

--- internal/commands/channel.go
+++ internal/commands/channel.go
@@ -1,6 +1,7 @@
 package commands
 
 import (
+	"errors"
 	"fmt"
 	"strconv"
 	"strings"
@@ -9,14 +10,64 @@ import (
 
 	"github.com/larsks/kwctl/internal/config"
 	"github.com/larsks/kwctl/internal/radio"
+	"github.com/larsks/kwctl/internal/types"
 )
 
 type (
-	ChannelCommand struct{}
+	ChannelCommand     struct{}
+	ChannelListCommand struct{}
+	ChannelEditCommand struct{}
 )
 
 func init() {
 	Register("channel", &ChannelCommand{})
+	Register("channel-list", &ChannelListCommand{}, "list")
+	Register("channel-edit", &ChannelEditCommand{}, "edit")
+}
+
+func (c ChannelListCommand) Run(r *radio.Radio, ctx config.Context, args []string) (string, error) {
+	for channel := range 999 {
+		var parsedChannel types.Channel
+		var channelName string
+
+		channelNum := fmt.Sprintf("%03d", channel)
+		res, err := r.SendCommand("ME", channelNum)
+		if err != nil {
+			if errors.Is(err, radio.ErrUnavailableCommand) {
+				parsedChannel = types.Channel{}
+			} else {
+				return "", fmt.Errorf("failed to list channels: %w", err)
+			}
+		} else {
+			parsedChannel, err = types.ParseChannel(res)
+			if err != nil {
+				return "", fmt.Errorf("invalid channel data: %w", err)
+			}
+
+			res, err := r.SendCommand("MN", channelNum)
+			if err != nil {
+				if !errors.Is(err, radio.ErrUnavailableCommand) {
+					return "", fmt.Errorf("failed to get channel name: %w", err)
+				}
+			} else {
+				parts := strings.SplitN(res, ",", 2)
+				if len(parts) == 2 {
+					channelName = parts[1]
+				}
+			}
+		}
+
+		if parsedChannel.RxFreq == 0 {
+			fmt.Printf("[%-6s] %03d\n", channelName, channel)
+		} else {
+			fmt.Printf("[%-6s] %s\n", channelName, parsedChannel)
+		}
+	}
+	return "", nil
+}
+
+func (c ChannelEditCommand) Run(r *radio.Radio, ctx config.Context, args []string) (string, error) {
+	return "", nil
 }
 
 func (c ChannelCommand) Run(r *radio.Radio, ctx config.Context, args []string) (string, error) {
