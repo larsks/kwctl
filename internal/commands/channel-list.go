b2f548ccb7245efad298bed2946b8b1c8f5599ca
Lars Kellogg-Stedman <lars@oddbit.com>
2025-11-13 23:44:27 -0500

    Fully implement channel list, edit commands using new channel api

---
+++ internal/commands/channel-list.go
@@ -0,0 +1,85 @@
+package commands
+
+import (
+	"errors"
+	"fmt"
+	"os"
+
+	flag "github.com/spf13/pflag"
+
+	"github.com/larsks/kwctl/internal/config"
+	"github.com/larsks/kwctl/internal/helpers"
+	"github.com/larsks/kwctl/internal/radio"
+	"github.com/larsks/kwctl/internal/types"
+)
+
+type (
+	ChannelListCommand struct {
+		flags *flag.FlagSet
+	}
+)
+
+func init() {
+	Register("channel-list", &ChannelListCommand{}, "list")
+}
+
+//nolint:errcheck
+func (c *ChannelListCommand) Init() error {
+	c.flags = flag.NewFlagSet("channel-list", flag.ContinueOnError)
+	c.flags.SetOutput(os.Stdout)
+	c.flags.Usage = func() {
+		fmt.Fprint(c.flags.Output(), helpers.Unindent(`
+			Usage: kwctl channel-list [options] <range> [<range> [...]]
+
+			List a range of channels.
+
+			Arguments:
+				range      A range specification (e.g. "1", "1-10", "1,5,10,15,20")
+
+			Options:
+			`))
+		c.flags.PrintDefaults()
+	}
+
+	return nil
+}
+
+func (c *ChannelListCommand) Run(r *radio.Radio, ctx config.Context, args []string) (string, error) {
+	if err := c.flags.Parse(args); err != nil {
+		return "", fmt.Errorf("command failed: %w", err)
+	}
+
+	var ranges []string
+	if c.flags.NArg() == 0 {
+		ranges = []string{"0-999"}
+	} else {
+		ranges = c.flags.Args()
+	}
+
+	for _, arg := range ranges {
+		for channelNumber, err := range helpers.RangeIterator(arg) {
+			if err != nil {
+				return "", fmt.Errorf("invalid range: %w", err)
+			}
+			if channelNumber < 0 || channelNumber > 999 {
+				return "", fmt.Errorf("invalid range (channels must be between 0 and 999)")
+			}
+
+			channel, err := r.GetMemoryChannel(channelNumber)
+			if err != nil {
+				if errors.Is(err, radio.ErrUnavailableCommand) {
+					channel = types.EmptyChannel
+				} else {
+					return "", fmt.Errorf("failed to list channels: %w", err)
+				}
+			}
+
+			if channel.RxFreq == 0 {
+				fmt.Printf("[%-6s] %03d\n", channel.Name, channelNumber)
+			} else {
+				fmt.Printf("[%-6s] %s\n", channel.Name, channel)
+			}
+		}
+	}
+	return "", nil
+}
--- internal/commands/channel.go
+++ internal/commands/channel.go
@@ -1,7 +1,6 @@
 package commands
 
 import (
-	"errors"
 	"fmt"
 	"os"
 	"strconv"
@@ -12,127 +11,16 @@ import (
 	"github.com/larsks/kwctl/internal/config"
 	"github.com/larsks/kwctl/internal/helpers"
 	"github.com/larsks/kwctl/internal/radio"
-	"github.com/larsks/kwctl/internal/types"
 )
 
 type (
 	ChannelCommand struct {
 		flags *flag.FlagSet
 	}
-	ChannelListCommand struct {
-		flags *flag.FlagSet
-	}
-	ChannelEditCommand struct {
-		flags *flag.FlagSet
-	}
 )
 
 func init() {
 	Register("channel", &ChannelCommand{})
-	Register("channel-list", &ChannelListCommand{}, "list")
-	Register("channel-edit", &ChannelEditCommand{}, "edit")
-}
-
-//nolint:errcheck
-func (c *ChannelListCommand) Init() error {
-	c.flags = flag.NewFlagSet("channel-list", flag.ContinueOnError)
-	c.flags.SetOutput(os.Stdout)
-	c.flags.Usage = func() {
-		fmt.Fprint(c.flags.Output(), helpers.Unindent(`
-			Usage: kwctl channel-list [options] <range> [<range> [...]]
-
-			List a range of channels.
-
-			Arguments:
-				range      A range specification (e.g. "1", "1-10", "1,5,10,15,20")
-
-			Options:
-			`))
-		c.flags.PrintDefaults()
-	}
-
-	return nil
-}
-
-func (c *ChannelListCommand) Run(r *radio.Radio, ctx config.Context, args []string) (string, error) {
-	if err := c.flags.Parse(args); err != nil {
-		return "", fmt.Errorf("command failed: %w", err)
-	}
-
-	for _, arg := range c.flags.Args() {
-		for channel, err := range helpers.RangeIterator(arg) {
-			if err != nil {
-				return "", fmt.Errorf("invalid range: %w", err)
-			}
-			if channel < 0 || channel > 999 {
-				return "", fmt.Errorf("invalid range (channels must be between 0 and 999)")
-			}
-			var parsedChannel types.Channel
-			var channelName string
-
-			channelNum := fmt.Sprintf("%03d", channel)
-			res, err := r.SendCommand("ME", channelNum)
-			if err != nil {
-				if errors.Is(err, radio.ErrUnavailableCommand) {
-					parsedChannel = types.Channel{}
-				} else {
-					return "", fmt.Errorf("failed to list channels: %w", err)
-				}
-			} else {
-				parsedChannel, err = types.ParseChannel(res)
-				if err != nil {
-					return "", fmt.Errorf("invalid channel data: %w", err)
-				}
-
-				res, err := r.SendCommand("MN", channelNum)
-				if err != nil {
-					if !errors.Is(err, radio.ErrUnavailableCommand) {
-						return "", fmt.Errorf("failed to get channel name: %w", err)
-					}
-				} else {
-					parts := strings.SplitN(res, ",", 2)
-					if len(parts) == 2 {
-						channelName = parts[1]
-					}
-				}
-			}
-
-			if parsedChannel.RxFreq == 0 {
-				fmt.Printf("[%-6s] %03d\n", channelName, channel)
-			} else {
-				fmt.Printf("[%-6s] %s\n", channelName, parsedChannel)
-			}
-		}
-	}
-	return "", nil
-}
-
-//nolint:errcheck
-func (c *ChannelEditCommand) Init() error {
-	c.flags = flag.NewFlagSet("channel-edit", flag.ContinueOnError)
-	c.flags.SetOutput(os.Stdout)
-	c.flags.Usage = func() {
-		fmt.Fprint(c.flags.Output(), helpers.Unindent(`
-			Usage: kwctl channel-edit [options] <channel>
-
-			Edit channel configuration.
-
-			Arguments:
-				channel    Channel number to edit (0-999)
-
-			Options:
-			`))
-		c.flags.PrintDefaults()
-	}
-	return nil
-}
-
-func (c *ChannelEditCommand) Run(r *radio.Radio, ctx config.Context, args []string) (string, error) {
-	if err := c.flags.Parse(args); err != nil {
-		return "", fmt.Errorf("command failed: %w", err)
-	}
-
-	return "", nil
 }
 
 //nolint:errcheck
