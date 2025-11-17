ba1ffb07cd9f0feb6aa5252ef819032b938e0750
Lars Kellogg-Stedman <lars@oddbit.com>
2025-11-16 20:05:33 -0500

    Add buildinfo command

---
+++ internal/commands/buildinfo.go
@@ -0,0 +1,34 @@
+package commands
+
+import (
+	"fmt"
+	"runtime/debug"
+
+	"github.com/larsks/kwctl/internal/config"
+	"github.com/larsks/kwctl/internal/radio"
+)
+
+type (
+	BuildinfoCommand struct{}
+)
+
+func init() {
+	Register("buildinfo", &BuildinfoCommand{})
+}
+
+func (c *BuildinfoCommand) NeedsRadio() bool {
+	return false
+}
+
+func (c *BuildinfoCommand) Init() error {
+	return nil
+}
+
+func (c *BuildinfoCommand) Run(r *radio.Radio, ctx config.Context, args []string) (string, error) {
+	bi, ok := debug.ReadBuildInfo()
+	if !ok {
+		return "", fmt.Errorf("unable to read buildinfo")
+	}
+	fmt.Printf("%s\n", bi)
+	return "", nil
+}
