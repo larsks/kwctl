c31ed686574fdd13b4f974d4cf6b5e5a25a54dd1
Lars Kellogg-Stedman <lars@oddbit.com>
2025-11-20 16:08:28 -0500

    Make target platform configurable in xc.sh

--- xc.sh
+++ xc.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
 KWBUILDER_IMAGE="${KWBUILDER_IMAGE:-kwbuilder}"
+KWBUILD_PLATFORM="${KWBUILDER_PLATFORM:-linux/arm64}"
 
 if ! podman image inspect "${KWBUILDER_IMAGE}" >/dev/null 2>&1; then
   echo "Generating ${KWBUILDER_IMAGE}"
-  podman build --platform linux/arm64 -t "${KWBUILDER_IMAGE}" -f builder/Containerfile builder
+  podman build --platform "${KWBUILD_PLATFORM}" -t "${KWBUILDER_IMAGE}" -f builder/Containerfile builder
 fi
 
-podman run -it --rm --platform linux/arm64 \
+podman run -it --rm --platform "${KWBUILD_PLATFORM}" \
   -v "$PWD":/src:z -w /src -v gocache:/cache -e GOMAXPROCS=10 \
   "${KWBUILDER_IMAGE}" "$@"
